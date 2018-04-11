97641dbd4db6345636d177e78cefc7d711d924d4
jerabaul29 <jean.rblt@gmail.com>
2018-04-11 15:36:56 +0200

    cleaned repo

--- Logger_GPS_SD_VN_Binary_output_Parser/test_process_folder.py
+++ Logger_GPS_SD_VN_Binary_output_Parser/test_process_folder.py
@@ -1,7 +1,7 @@
 import parser_logger
 
-path_to_data = "/media/hydroubuntu/29AE-4639/"
-path_to_output = "/home/hydroubuntu/Desktop/Current/TestsParser/"
+path_to_data = "/home/jrlab/Desktop/Git/LoggerWavesInIce/Logger_GPS_SD_VN_Binary_output_Parser/Example_data/"
+path_to_output = "/home/jrlab/Desktop/Git/LoggerWavesInIce/Logger_GPS_SD_VN_Binary_output_Parser/Example_parsed_data/"
 verbose = 0
 
 parser = parser_logger.Parser_logger(path=path_to_data, path_output=path_to_output)
